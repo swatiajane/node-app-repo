2ad03fd144458cabd3b2bf646c1244b3310e0a6f
swatiajane <swatiajane66@gmail.com>
2024-09-17 21:08:42 +0530

    Assigment-1
    Assigment-1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-app-repo
 This is Node App Repository
+
+# project build steps
+1.Clone the Repository - <repo-url> 
---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("my first app");
+console.log(`File Name: ${__filename}`);
+console.log(`Directory Name: ${__dirname}`);
+
+const os = require('os');
+
+ 
+
+console.log('My first app');
+
+console.log(`File Name: ${__filename}`);
+
+console.log(`Directory Name: ${__dirname}`);
+
+ 
+
+console.log("=============== Built in Module - OS =======================");
+
+console.log(`Arch: ${os.arch()}`);

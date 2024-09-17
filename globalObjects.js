52b5569361ee5833ac1b18731edc2a74d951e3b9
swatiajane <swatiajane66@gmail.com>
2024-09-17 22:02:47 +0530

    Globalobjects
    Globalobjects

---
+++ globalObjects.js
@@ -0,0 +1,6 @@
+console.log(global);
+console.log(console);
+console.log(process);
+console.log(_filename);
+console.log(_dirname);
+console.log(module);
---
+++ os.js
@@ -0,0 +1,11 @@
+const os = required('os');
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalname());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.userInfo());
